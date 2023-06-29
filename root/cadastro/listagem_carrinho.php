6c2b7729a9d444e57ee1e6ee487c61442fb356b2
EGAMER26 <evandrogomes542@gmail.com>
2023-06-29 11:03:53 -0300

    feita a atualização na listagem, aonde o valor exibido é o do banco de dados, não a da variavel armazenada na seção

--- root/cadastro/listagem_carrinho.php
+++ root/cadastro/listagem_carrinho.php
@@ -9,20 +9,18 @@
     }
     $logado = $_SESSION['email'];
     // $valorTotalProdutos = $_SESSION['preco_total_produtos'];
-
-    $query = "SELECT total_carrinho FROM cadastro_usuario WHERE email = '$email_logado'";
-$resultado = mysqli_query($conexao, $query);
+    include ('conexao_banco_eos.php');
+    $query = "SELECT total_carrinho FROM cadastro_usuario WHERE email = '$logado'";
+    $resultado = mysqli_query($conexao, $query);
 
 if ($resultado && mysqli_num_rows($resultado) > 0) {
     $row = mysqli_fetch_assoc($resultado);
     $totalCarrinho = $row['total_carrinho'];
 
-    // Use a variável $totalCarrinho conforme necessário
-    echo "O valor total do carrinho é: " . $totalCarrinho;
 } else {
-    echo "Não foi possível recuperar o valor total do carrinho.";
+    echo "Não foi possível recuperar o valor total do carrinho." ;
 }
-    $valorFormatado = number_format($valorTotalProdutos, 2, ',', '.');
+    $valorFormatado = number_format($totalCarrinho, 2, ',', '.');
     $comCifrao = 'R$ ' . $valorFormatado;
 ?>
 <?php
